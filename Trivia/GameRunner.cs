4df3e14d2f0f92ddc7eaa449e9538f53edff07c1
Christina Ripley-Gonzalez <christina.ripley@codurance.com>
2017-11-21 14:51:24 +0000

    moved Test files into embedded resources

--- Trivia/GameRunner.cs
+++ Trivia/GameRunner.cs
@@ -14,17 +14,28 @@ namespace Trivia
 
         public static void Main(String[] args)
         {
+            PlayGame(null);
+        }
+
+        public static void PlayGame(int? randomSeed)
+        {
             Game aGame = new Game();
 
             aGame.add("Chet");
             aGame.add("Pat");
             aGame.add("Sue");
-
-            Random rand = new Random(4);
+            Random rand;
+            if (randomSeed.HasValue)
+            {
+                rand = new Random(randomSeed.Value);
+            }
+            else
+            {
+                rand = new Random();
+            }
 
             do
             {
-
                 aGame.roll(rand.Next(5) + 1);
 
                 if (rand.Next(9) == 7)
@@ -35,14 +46,8 @@ namespace Trivia
                 {
                     notAWinner = aGame.wasCorrectlyAnswered();
                 }
-
-
-
             } while (notAWinner);
-
         }
-
-
     }
 
 }
